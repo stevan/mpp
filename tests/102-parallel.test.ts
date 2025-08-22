8a2c742bcd31cee2f29da86634000776e3b7597e
Stevan Little <stevan.little@gmail.com>
2025-08-22 17:45:56 +0200

    parallel loop

---
+++ tests/102-parallel.test.ts
@@ -0,0 +1,24 @@
+
+import { Source } from '../src/Assembler'
+import { Instruction } from '../src/InstructionSet'
+import { interpret } from '../src/Parallel'
+
+// 10 BEGIN DUP 1 - DUP 0 == UNTIL
+let Countdown : Source = [
+    [ Instruction.ENTER, [] ],
+    [ Instruction.CONST, [ 10 ] ],
+// TOP OF LOOP
+    [ Instruction.DUP,   []    ],
+    [ Instruction.CONST, [ 1 ] ],
+    [ Instruction.SUB,   []    ],
+
+    [ Instruction.DUP,   []    ],
+    [ Instruction.CONST, [ 1 ] ],
+    [ Instruction.EQ,    []    ],
+
+    [ Instruction.JWZ,   [ 2 ] ], // jump to TOP OF LOOP
+
+    [ Instruction.LEAVE, [] ],
+];
+
+interpret(Countdown, 1);
